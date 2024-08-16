86ff9567a6327dd50e4c8e75550243118b2913b7
dirttop <gsyomichev@gmail.com>
2024-08-16 16:26:46 -0400

    Scalable Objects!

--- Testing/Scenes and Scripts/object.gd
+++ Testing/Scenes and Scripts/object.gd
@@ -1,15 +1,31 @@
 extends RigidBody2D
 
+@export var scale_limit = .3
+@export var base_scale = .5
+var shrink_scale
+var enlarge_scale
 
-# Called when the node enters the scene tree for the first time.
+var scaling = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	shrink_scale = base_scale - scale_limit
+	enlarge_scale = base_scale + scale_limit
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
-
+	if scaling:
+		if Global.ray_mode == "shrink":
+			if not($CollisionShape2D.scale.x < shrink_scale) :
+				$CollisionShape2D.scale -= Vector2(.1*delta, .1*delta)
+				$Sprite2D.scale -= Vector2(.1*delta, .1*delta)
+				$Area2D/CollisionShape2D.scale -= Vector2(.1*delta, .1*delta)
+		if Global.ray_mode == "enlarge":
+			if not($CollisionShape2D.scale.x > enlarge_scale) :
+				$CollisionShape2D.scale += Vector2(.1*delta, .1*delta)
+				$Sprite2D.scale += Vector2(.1*delta, .1*delta)
+				$Area2D/CollisionShape2D.scale += Vector2(.1*delta, .1*delta)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("here")
+	scaling = true
+	
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	scaling = false
