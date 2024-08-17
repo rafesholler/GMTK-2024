296e8fc6343738fd258c4920541f9eb738986237
Rafe Sholler <rafe.sholler@gmail.com>
2024-08-17 16:21:07 -0400

    Updated ant movement

--- characters/tiny/ant.gd
+++ characters/tiny/ant.gd
@@ -21,4 +21,14 @@ func _physics_process(delta: float) -> void:
 		$RayCast2D.position.x *= -1
 	velocity.x = speed * direction
 	
+	#rotate the ant based off the slope of the ground
+	if $SlopeRayCast.is_colliding():
+		var vec = $SlopeRayCast.get_collision_normal()
+		var rot = vec.angle_to(Vector2(0, -1)) * -1
+		rotation = rot
+		if velocity.x != 0 and velocity.y == 0 and abs(rot) > 4*PI/9:
+			velocity = velocity.rotated(rot)
+		print(velocity)
+		print(rot)
+	
 	move_and_slide()
